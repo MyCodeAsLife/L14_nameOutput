0d2fe58c2bea150ed680acf64c2a18b142a6fee1
Vladimir <w112vovan@mail.ru>
2023-11-27 13:09:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L14_nameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            char userSymbol;
+            int identation = 4;
+
+            Console.Write("Введите ваше имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Введите желаемый символ: ");
+            userSymbol = Convert.ToChar(Console.Read());
+
+            Console.Clear();
+            Console.WriteLine(new string(userSymbol, userName.Length + identation));
+            Console.WriteLine($"{userSymbol} " + userName + $" {userSymbol}");
+            Console.WriteLine(new string(userSymbol, userName.Length + identation));
+        }
+    }
+}
